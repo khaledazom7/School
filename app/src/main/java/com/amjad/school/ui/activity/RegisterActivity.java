399ebd4ef91956ad2af81141c4347507dae9ae1e
khaled <khaledazo7@gmail.com>
2022-01-27 17:48:06 +0200

    School.home xml

--- app/src/main/java/com/amjad/school/ui/activity/RegisterActivity.java
+++ app/src/main/java/com/amjad/school/ui/activity/RegisterActivity.java
@@ -10,11 +10,17 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.amjad.school.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class RegisterActivity extends AppCompatActivity {
-    EditText nameEditeText,emailEditeText,passwordEditeText;
-    String name,email,password;
-    Button registerButton;
+   private EditText nameEditeText,emailEditeText,passwordEditeText;
+   private String name,email,password;
+   private Button registerButton;
+
+
+   FirebaseAuth firebaseAuth;
+   FirebaseFirestore firebaseFirestore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,30 +29,79 @@ public class RegisterActivity extends AppCompatActivity {
         emailEditeText = findViewById(R.id.editTextTextEmailAddress);
         passwordEditeText = findViewById(R.id.editTextTextPassword);
         registerButton = findViewById(R.id.buttonregister);
-        name = nameEditeText.getText().toString().trim();
-        email = emailEditeText.getText().toString().trim();
-        password = passwordEditeText.getText().toString().trim();
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
 
 
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
+                name = nameEditeText.getText().toString().trim();
+                email = emailEditeText.getText().toString().trim();
+                password = passwordEditeText.getText().toString().trim();
+                checkInputInfo();
                 if(count==3) {
-
-                    checkInputInfo();
+                    registerUserByEmailAndPassword(email,password);
+                    firebaseAuth.createUserWithEmailAndPassword(userEmail, userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                addNewUserOnDbFirebase();
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Failed to register! Try again!", Toast.LENGTH_SHORT).show();
+                            }
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
                 }
+
+                private void addNewUserOnDbFirebase() {
+                    firebaseUser = firebaseAuth.getCurrentUser();
+                    User user = new User(userName, userEmail, "", "", "", "", userType, dateOfCreate, "https://i.ibb.co/W0hVGcJ/accont.png", true, "");
+                    // save on cloudFireStore
+                    DocumentReference documentReference = firebaseFirestore.collection("Users")
+                            .document(firebaseUser.getUid());
+                    documentReference.set(user);
+                    verifyEmail();
+                }
+
+                private void verifyEmail() {
+                    FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+                    assert firebaseUser != null;
+                    firebaseUser.sendEmailVerification();
+                    Toast.makeText(getApplicationContext(), "Registered Successfully!\nCheck your email to verify your account!", Toast.LENGTH_LONG).show();
+                }
+
+
+
+
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+finish();
+                }
+            }
+
+            private void registerUserByEmailAndPassword(String email, String password) {
             }
         });
 
 
 }
    int count =3;
-private int checkInputInfo(){
+
+private void checkInputInfo(){
     count =3;
     if (name.isEmpty()) {
         nameEditeText.setError("Yuor name is Required");
         nameEditeText.hasFocus();
-        count --;
+        count--;
         return;
     }
     if (email.isEmpty()) {
